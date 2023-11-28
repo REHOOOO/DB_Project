2218a959ac8a9cd04bd09383139960e5c48aceb7
REHOOOO <ryueunhwan00@gmail.com>
2023-11-28 13:15:04 +0900

    Project
    input_info, ocr 함수 생성
    참고링크 생성

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# 샘플 Python 스크립트입니다.
+import requests
+import uuid
+import time
+import base64
+import json
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+def input_info(): # 사용자 정보를 입력받아 리턴하는 함수
+    height = float(input("키를 입력하세요 (cm): "))
+    weight = float(input("몸무게를 입력하세요 (kg): "))
+    age = int(input("나이를 입력하세요: "))
+    return height, weight, age
 
+def ocr(file_path):
+    api_url = 'https://2bwclle49c.apigw.ntruss.com/custom/v1/26532/9032c8f9fe48076d9b1fe6ee6c9f0e47170cb4cb33e1df43afac3fa35ad1f3c5/general'
+    secret_key = 'QlVZenhLZHN1VHNtSGlkcXplY0VuZm9SWERLWVBMVVQ='
+    image_file = ''
+    with open(image_file,'rb') as f:
+        file_data=f.read()
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+    request_json = {
+        'images':[
+            {
+                'format': 'jpg',
+                'name': 'demo',
+                'data': base64.b64encode(file_data).decode()
+            }
+        ],
+        'requestId': str(uuid.uuid4()),
+        'version': 'V2',
+        'timestamp': int(round(time.time() * 1000))
+    }
 
+    payload = json.dumps(request_json).encode('UTF-8')
+    headers = {
+        'X-OCR-SECRET': secret_key,
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", api_url, headers=headers,data=payload)
+    return response
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    height, weight, age = input_info()
+    response = ocr()
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
